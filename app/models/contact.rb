778e596597a275a4f1d4b8e86c4a02d5e8880c5e
brahian609 <brahian609@hotmail.com>
2016-07-14 15:24:40 -0500

    feat(contact.rb) add comment important

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  has_many :phone_numbers
+  has_many :phone_numbers # add dependent: :destroy para eliminar completamente los registros relacionados de lo contrario deja el campo null
 
   ### Validations
   validates :name_first, presence: true
