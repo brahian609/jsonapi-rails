8d882425e72b9de6ed1b22a2567cd948d4066df3
brahian609 <brahian609@hotmail.com>
2016-07-14 16:48:09 -0500

    feat(application_controller) add method PATCH to CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   def cors_preflight_check
     if request.method == "OPTIONS"
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH'
       headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'application/json'
